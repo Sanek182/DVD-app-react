496982639647079c5db6df53d0a8c0504762131c
Sanek182 <maxpower_90>
2023-07-20 21:55:27 +0300

    initial pages and first code

---
+++ src/pages/main-page/MainPage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import DVDcard from '../../components/dvd-card/DVDcard';
+import { fetchDVDData } from '../../filler/api';
+import Background from '../../components/static/Background'
+
+function MainPage() {
+    const [dvds, setDVDs] = useState([]);
+    const movieID = [22257];
+
+    useEffect(() => {
+        const fetchDVDs = async () => {
+            const fetchedDVDs = [];
+            
+            for (const id of movieID) {
+                const dvdData = await fetchDVDData(id);
+                if (dvdData) {
+                fetchedDVDs.push(dvdData);
+                }
+            }
+        
+            setDVDs(fetchedDVDs);
+        };
+    
+        fetchDVDs();
+      }, []);
+    
+    return (
+        <div>
+            <Header />
+            <Background />
+            {dvds.map(dvd => (
+                <DVDcard key={dvd.id} dvd={dvd} />
+            ))}
+            <Footer />
+        </div>
+    )
+}
+
+export default MainPage;
---
+++ src/pages/product-page/ProductPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchDVDData } from "../../filler/api";
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+
+function ProductPage() {
+    let { id } = useParams();
+    const [ dvd, setDVD ] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const dvdData = await fetchDVDData(id);
+            setDVD(dvdData);
+        };
+    
+        fetchData();
+    }, [id]);
+
+    return (
+        <div>
+            <Header />
+            { dvd ? (
+                <>
+                    <h1>{dvd.title}</h1>
+                    <h2>{dvd.release_date.slice(0, 4)}</h2>
+                    <p>{dvd.genres.map(genre => genre.name).join(', ')}</p>
+                    <p>{dvd.production_countries.map(country => country.name).join(', ')}</p>
+                    <p>{dvd.vote_average}</p>
+                    <p>{dvd.overview}</p>
+                </>
+            ) : (
+                "Loading..."
+            )}
+            <Footer />
+        </div>
+    )
+}
+
+export default ProductPage;
