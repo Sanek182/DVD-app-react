c7c33d255a1baff0a801c3c823de7b460cd707ed
Sanek182 <maxpower_90>
2023-08-22 13:33:15 +0300

    minor changes

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loginUser = async (username, password) => {
   try {
-    const response = await axios.post('/auth/login', { username, password });
+    const response = await axios.post('http://localhost:3500/auth/login', { username, password });
     return response.data;
   } catch (error) {
     console.error(error);
@@ -12,7 +12,7 @@ export const loginUser = async (username, password) => {
 
 export const registerUser = async (username, email, password) => {
   try {
-    const response = await axios.post('/auth/register', { username, email, password });
+    const response = await axios.post('http://localhost:3500/auth/register', { username, email, password });
     return response.data;
   } catch (error) {
     console.error(error);
--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -5,42 +5,48 @@ import { loginUser } from '../../api/authAPI';
 import "./LoginPage.css";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    try {
-      const response = await loginUser(username, password);
-      if (response.success) {
-        navigate('/');
-      } else {
-        setErrorMessage(response.error || "Login failed");
-      }
-    } catch (error) {
-      setErrorMessage("An error occurred. Please try again later.");
-    }
-  };
+    const handleLogin = async () => {
+        if (username.trim() === "" || password.trim() === "") {
+            setErrorMessage("Please enter all the required fields.");
+            return;
+        }
 
-  return (
-    <div className="login-container">
-      <button onClick={() => navigate('/')}>Close</button>
-      <div className="login-section">
-        <h2>Please login</h2>
-        <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-        <button onClick={handleLogin}>Login</button>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <div className="registration-links">
-          <p>Do not have an account on our site yet?</p>
-          <button onClick={() => navigate('/auth/registration')}>Registration</button>
-          <button onClick={() => {/* ... */}}>Forgot password?</button>
+        try {
+            const response = await loginUser(username, password);
+            if (response.success) {
+            alert('You have successfully logged in!');
+            navigate('/');
+            } else {
+            setErrorMessage("Such user does not exist. Please check your inputs once again.");
+            }
+        } catch (error) {
+            setErrorMessage("An error occurred. Please try again later.");
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <button onClick={() => navigate('/')}>Close</button>
+            <div className="login-section">
+                <h2>Please login</h2>
+                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleLogin}>Login</button>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                <div className="registration-links">
+                <p>Do not have an account on our site yet?</p>
+                <button onClick={() => navigate('/auth/registration')}>Registration</button>
+                <button onClick={() => {/* ... */}}>Forgot password?</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
